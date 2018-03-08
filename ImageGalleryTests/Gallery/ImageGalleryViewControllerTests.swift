c8ff2f98e2c2fbbba5325d94fd5834b7758c772c
Arslan Raza <arslan_raza21@hotmail.com>
2018-03-08 23:49:31 +1100

    - adds a failing test for ImageGallery to check if it has a collectionView after being displayed

---
+++ ImageGalleryTests/Gallery/ImageGalleryViewControllerTests.swift
@@ -0,0 +1,9 @@
+//
+//  ImageGalleryViewControllerTests.swift
+//  ImageGalleryTests
+//
+//  Created by Arslan Raza on 8/3/18.
+//  Copyright © 2018 arkTechs. All rights reserved.
+//
+
+import Foundation
