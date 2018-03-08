a4a868077d02d17f66b7c980a051d852c27e3032
Arslan Raza <arslan_raza21@hotmail.com>
2018-03-08 22:57:58 +1100

    - refactors APIClientTests to inject MockURLSessino and MockURLDataTask

--- SparkImageNetworkTests/NetworkingTests/APIClientTests.swift
+++ SparkImageNetworkTests/NetworkingTests/APIClientTests.swift
@@ -11,6 +11,40 @@ import Quick
 import Nimble
 @testable import SparkImageNetwork
 
+class MockURLSession: URLSessionProtocol {
+  
+  private(set) var invalidateCalled = false
+  var nextDataTask = MockURLSessionDataTask()
+  var nextData: Data?
+  var nextError: Error?
+  
+  private (set) var lastURL: URL?
+  
+  func successHttpURLResponse(request: URLRequest) -> URLResponse {
+    return HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: nil)!
+  }
+  
+  func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol {
+    lastURL = request.url
+    
+    completionHandler(nextData, successHttpURLResponse(request: request), nextError)
+    return nextDataTask
+  }
+  
+  func invalidateAndCancel() {
+    invalidateCalled = true
+  }
+  
+}
+
+class MockURLSessionDataTask: URLSessionDataTaskProtocol {
+  private (set) var resumeWasCalled = false
+  
+  func resume() {
+    resumeWasCalled = true
+  }
+}
+
 struct MockObject: Codable {
   let name: String
 }
@@ -18,38 +52,50 @@ struct MockObject: Codable {
 class MockAPI: APIClient {
   var session: URLSessionProtocol
   
-  init() {
-   self.session = URLSession(configuration: .default)
+  init(with expectedData: Data?, expectedError: Error?) {
+    let mockSession = MockURLSession()
+    mockSession.nextData = expectedData
+    mockSession.nextError = expectedError
+    self.session = mockSession
   }
   
-  func fetch(with request: URLRequest, decode: @escaping (Decodable) -> MockObject?, completion: @escaping (Result<MockObject, APIError>) -> Void) {
-    let mockObject = MockObject(name: "mock")
-    completion(.success(mockObject))
-  }
-
 }
 
 class APIClientTests: QuickSpec {
   override func spec() {
     describe("APIClient") {
-      context("after initializing", {
+      
+      context("after initializing with data", {
         
-        let api = MockAPI()
+        let data = "{\"name\": \"test\"}".data(using: .utf8)
+        
+        let api = MockAPI(with: data, expectedError: nil)
         var success = false
-        api.fetch(with: URLRequest.init(url: URL(string: "http://someurl.com")!), decode: { object -> Decodable? in
-          return object
-        }, completion: { result in
-          switch result {
-          case .success(_):
-            success = true
-          case .failure(_):
-            print("error: ")
-          }
+        let request = URLRequest(url: URL(string: "http://someurl.com")!)
+        
+        it("should return object", closure: {
+          api.fetch(with: request, decode: { json -> MockObject? in
+            guard let object = json as? MockObject else {
+              return  nil
+            }
+            return object
+          }, completion: { result in
+            switch result {
+            case .success(let object):
+              success = object.name == "test"
+            case .failure(_):
+              print("error: ")
+            }
+          })
+          expect(success).toEventually(beTrue())
         })
         
-        it("should pass", closure: {
-          expect(success).to(beTrue())
+        it("should cancel if invalidate is called", closure: {
+          let mockSession = api.session as! MockURLSession
+          mockSession.invalidateAndCancel()
+          expect(mockSession.invalidateCalled).to(beTrue())
         })
+        
       })
     }
   }
