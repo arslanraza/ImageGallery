9a6edfce07f9893b8d59d7b2f1d03ccf91671e42
Arslan Raza <arslan_raza21@hotmail.com>
2018-03-08 23:56:41 +1100

    - Adds GalleryViewModel and its test for clean initial state.

---
+++ ImageGallery/Gallery/ViewModel/GalleryViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  GalleryViewModel.swift
+//  ImageGallery
+//
+//  Created by Arslan Raza on 8/3/18.
+//  Copyright © 2018 arkTechs. All rights reserved.
+//
+
+import Foundation
+import SparkImageGalleryCore
+
+class GalleryViewModel {
+  var pictures: [Picture] = []
+}
