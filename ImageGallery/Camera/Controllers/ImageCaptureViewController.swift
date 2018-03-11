bdea7388655eb8630f22b571690a841ed75d278d
Arslan Raza <arslan_raza21@hotmail.com>
2018-03-11 14:09:53 +1100

    - Adds pods for custom ImagePickerController and CropViewController - Updates ImageCaptureViewController to inherit from ImagePickerViewController - Adds CropViewController delegate methods in ImageCaptureViewController - Removes the unused default test file ImageGalleryTests.swift

--- ImageGallery/Camera/Controllers/ImageCaptureViewController.swift
+++ ImageGallery/Camera/Controllers/ImageCaptureViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import ImagePicker
+import CropViewController
 
-class ImageCaptureViewController: UIViewController {
+class ImageCaptureViewController: ImagePickerController {
+  
+  convenience init() {
+    var configuration = Configuration()
+    configuration.doneButtonTitle = "Done"
+    configuration.noImagesTitle = "Sorry! There are no images here!"
+    configuration.recordLocation = false
+    self.init(configuration: configuration)
+    
+    self.imageLimit = 1
+    self.delegate = self
+    
+  }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.setNavigationBarHidden(true, animated: false)
+  }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -32,4 +51,29 @@ class ImageCaptureViewController: UIViewController {
     }
     */
 
+}
+
+extension ImageCaptureViewController: CropViewControllerDelegate {
+  func cropViewController(_ cropViewController: CropViewController, didFinishCancelled cancelled: Bool) {
+    navigationController?.popViewController(animated: true)
+  }
+  func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+    cropViewController.dismiss(animated: true, completion: nil)
+  }
+}
+
+extension ImageCaptureViewController: ImagePickerDelegate {
+  func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+    
+  }
+  
+  func cancelButtonDidPress(_ imagePicker: ImagePickerController) {
+    imagePicker.dismiss(animated: true, completion: nil)
+  }
+  
+  func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+    let cropController = CropViewController(croppingStyle: CropViewCroppingStyle.default, image: images.first!)
+    cropController.delegate = self
+    navigationController?.pushViewController(cropController, animated: true)
+  }
 }
--- Pods/Target Support Files/Pods-ImageGallery/Pods-ImageGallery-frameworks.sh
+++ Pods/Target Support Files/Pods-ImageGallery/Pods-ImageGallery-frameworks.sh
@@ -100,6 +100,15 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CropViewController/CropViewController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ImagePicker/ImagePicker.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CropViewController/CropViewController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ImagePicker/ImagePicker.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
